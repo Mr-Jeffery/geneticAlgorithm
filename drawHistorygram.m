20fa10030147f9d2ba1b8d1e17b45617a903d98a
Jeffery <94628699+Mr-Jeffery@users.noreply.github.com>
2023-12-03 21:47:51 +0800

    Update drawHistorygram.m

--- drawHistorygram.m
+++ drawHistorygram.m
@@ -6,3 +6,16 @@ surf(X,Y,HistorygramE,EdgeColor="none")
 subplot(1,2,2)
 surf(X,Y,HistorygramK,EdgeColor="none")
 %bar3(HistorygramK)
+
+% figure(5)
+% [X,Y] = meshgrid(1:t,1:n);
+% colormap jet
+% subplot(1,2,1)
+% surf(X,Y,HistorygramE,EdgeColor="none")
+% subplot(1,2,2)
+% surf(X,Y,HistorygramK,EdgeColor="none")
+% %bar3(HistorygramK)
+% contour3(HistorygramE_GPU(60:140,1:1e5))
+% set(gcf,'RendererMode','auto')
+% set(gcf,"Renderer","opengl")
+% set(gca,'Color','k')
