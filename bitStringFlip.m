acf1a1bc061f259f67393a87056786a975a11482
Jeffery <94628699+Mr-Jeffery@users.noreply.github.com>
2023-12-04 12:42:41 +0800

    Update bitStringFlip.m

--- bitStringFlip.m
+++ bitStringFlip.m
@@ -1,6 +1,6 @@
 function newX = bitStringFlip(X,p)
 % p is the probability of filping, p \in [0,1]
-Choice = rand(size(X))>p;
-newX = ~X; %Flip all bits first
-newX(Choice) = X(Choice); %Keep the bits that should be kept
+Choice = rand(size(X))<p;
+newX = X; %Flip all bits first
+newX(Choice) = ~X(Choice); %Keep the bits that should be kept
 end
